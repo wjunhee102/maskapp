d2ed25410a597273dd6507750cd1db34a4fbce1e
wjunhee102 <wjunhee102@gmail.com>
2020-04-17 17:49:33 +0900

    카카오 맵 추가

--- src/kakao/kakaoMap.js
+++ src/kakao/kakaoMap.js
@@ -6,19 +6,19 @@ const { kakao } = window;
 
 function KakaoMap() {
     
-    useEffect(()=>{
+    // useEffect(()=>{
         
-        kakao.maps.load(()=>{
+    //     kakao.maps.load(()=>{
             
-            const container = document.getElementById("map");
-            const options = {
-                center : new kakao.maps.LatLng(33.450701, 126.570667),
-                level: 5
-            }
-            const map = new kakao.maps.Map(container, options)
-        },[kakao.maps]);
+    //         const container = document.getElementById("map");
+    //         const options = {
+    //             center : new kakao.maps.LatLng(33.450701, 126.570667),
+    //             level: 5
+    //         }
+    //         const map = new kakao.maps.Map(container, options)
+    //     },[kakao.maps]);
 
-    });
+    // });
 
     // useEffect(() => { 
     //     const container = document.getElementById('map'); 
@@ -27,16 +27,16 @@ function KakaoMap() {
     // }, []);
 
 
-    // useEffect(() => {
-    //     console.log(kakao)
-    //     kakao.maps.load(() => {
-    //         let el = document.getElementById('map');
-    //         let map = new kakao.maps.Map(el, {
-    //             center: new kakao.maps.Coords(523951.25, 1085073.75)
-    //         })
-    //     })
+    useEffect(() => {
+        console.log(kakao)
+        kakao.maps.load(() => {
+            let el = document.getElementById('map');
+            let map = new kakao.maps.Map(el, {
+                center: new kakao.maps.Coords(523951.25, 1085073.75)
+            })
+        })
 
-    // }, [kakao.maps]);
+    }, [kakao.maps]);
     
 
     return(
